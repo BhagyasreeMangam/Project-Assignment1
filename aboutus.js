7c54eff1c3ac504ed3c8b7df90ff0dbe87205364
BhagyasreeMangam <96426908+BhagyasreeMangam@users.noreply.github.com>
2021-12-21 22:25:44 +0530

    Add files via upload

---
+++ aboutus.js
@@ -0,0 +1,21 @@
+
+let hospElement = document.getElementById("vjyBtn");
+let hospElement1 = document.getElementById("hydBtn");
+let hospElement2 = document.getElementById("mbBtn");
+let hospElement3 = document.getElementById("bglBtn");
+
+
+
+hospElement.addEventListener('click',()=>{
+    document.body.style.backgroundImage="url('https://image.shutterstock.com/image-photo/large-modern-office-building-600w-148701419.jpg')";
+});
+hospElement1.addEventListener('click',()=>{
+    document.body.style.backgroundImage="url('https://image.shutterstock.com/image-photo/riviere-du-rempartmauritius-august-15-600w-1477471940.jpg')";
+});
+hospElement2.addEventListener('click',()=>{
+    document.body.style.backgroundImage="url('https://image.shutterstock.com/image-photo/toronto-canada-august-7-2019-600w-1473229451.jpg')";
+});
+hospElement3.addEventListener('click',()=>{
+    document.body.style.backgroundImage="url('https://image.shutterstock.com/image-photo/delft-netherlands-august-2019-online-600w-1481499938.jpg')";
+});
+
